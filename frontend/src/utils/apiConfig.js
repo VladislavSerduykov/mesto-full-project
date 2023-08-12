29b0c0eb5117f16cb6c96fbcf820237188092761
Vladislav Serdyukov <pandaisback62@mail.ru>
2023-08-12 23:50:45 +0300

    Убрал токен

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,9 +1,6 @@
-const token = "d66689bd-a4b2-435a-b216-8a223a62a546";
-
 export const config = {
   baseUrl: 'https://api.vladislav.student.nomoparties.co',
   headers: {
-    authorization: token,
     "Content-Type": "application/json",
   },
 };
