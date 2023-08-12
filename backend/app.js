db834a063e451b5a8e700db0220c5260a2277a54
Vladislav Serdyukov <pandaisback62@mail.ru>
2023-08-12 22:47:38 +0300

    Добавил логеры, установил CORS

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const { handleError } = require('./middlewares/handleError');
 
 const { routes } = require('./routes/route');
+const { errorLogger, requestLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -31,9 +33,15 @@ mongoose
 
 app.use(limiter);
 
+app.use(cors());
+
+app.user(requestLogger);
+
 app.use('*', express.json());
 
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(handleError);
